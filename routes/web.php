ff1e73a97704654c95027e16a381c493453f8025
ahmeddaryaofficials <ahmeddaryaofficials@gmail.com>
2022-09-12 23:19:16 +0500

    first commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\CaategoryControllers;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+ Route::middleware(['auth', 'isAdmin'])->group(function () {
+    Route::get('/dashboard',  'Admin\FrontendController@index');
+    Route::get('/categories',  'Admin\CategoryController@index');
+    Route::get('/add_category',  'Admin\CategoryController@add');
+    Route::post('/insert_category','Admin\CategoryController@insert');
+    Route::get('edit-product/{id}', [CategoryController::class,'edit']);
+ });
